b2b232cc81b6b8aaeced22a2bf6cca00fbe33958
bufan <617620901@qq.com>
2022-08-10 11:18:19 +0800

    8.10 AM

--- src/main/java/com/shf/spring/sms/task/CheckThing/rule/WriteClearlySelectFieldRule.java
+++ src/main/java/com/shf/spring/sms/task/CheckThing/rule/WriteClearlySelectFieldRule.java
@@ -1,9 +1,14 @@
 package com.shf.spring.sms.task.CheckThing.rule;
 
+import net.sf.jsqlparser.statement.select.SelectVisitor;
 import com.shf.spring.sms.task.ASTNode.AST;
 import com.shf.spring.sms.task.ASTNode.SqlTypes;
 import com.shf.spring.sms.task.PrintResult.Report;
+import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.expression.ExpressionVisitorAdapter;
+import net.sf.jsqlparser.statement.select.*;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -12,15 +17,59 @@ import java.util.List;
 
 public class WriteClearlySelectFieldRule implements CheckRule{
     @Override
-    public Report match(AST tree) {
-//        Report report = new Report(tree.getSql());
-        // 抛出异常
-        throw new RuntimeException("异常");
+    public Report match(AST tree)
+    {
+        Report report = new Report(tree.getSql());
+//        // 抛出异常
+//        throw new RuntimeException("异常");
+        report.setPass(true);
+
+        List<SelectItem> selectItems = tree.getSelects();
+        //查询体中是否有*号
+        if(checkAsterisk(selectItems)){
+            report.setDesc("请写明查询字段，不要使用select *");
+            report.setPass(false);
+            report.setLevel(Report.Level.ERROR);
+            return report;
+        }
+
+        //join子查询中是否有*号，有则报错
+        List<Join> joins = tree.getJoins();
+        if(joins == null || joins.size() <1){
+            return report;
+        }
+
+        for(Join join : joins){
+            //如果是子查询
+            if(join.getRightItem() instanceof SubSelect){
+                //获取子查询
+                SelectBody selectBody = ((SubSelect) join.getRightItem()).getSelectBody();
+                if(selectBody instanceof PlainSelect){
+                    //检查是否有*号
+                    if(checkAsterisk(((PlainSelect) selectBody).getSelectItems())){
+                        report.setDesc("请写明查询字段，不要使用select *");
+                        report.setPass(false);
+                        report.setLevel(Report.Level.ERROR);
+                        return report;
+                    }
+                }
+            }
+        }
+
+        //where子查询中是否有*号
+        Expression where = tree.getWhere();
+        ExpressionVisitorAdapter adapter = new ExpressionVisitorAdapter();
+        adapter.setSelectVisitor( new MySelectVisitor(report));
+        where.accept(adapter);
+        return report;
+    }
+
+    private boolean checkAsterisk(List<SelectItem> selectItems) {
     }
 
     @Override
     public List<SqlTypes> scope()
     {
-        return null;
+        return Arrays.asList(SqlTypes.SELECT);
     }
 }
