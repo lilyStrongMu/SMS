590a30cfa810e3b21e5c0f6527342dfda735fe9a
gaominqi1998 <gaominqi@bupt.edu.cn>
2022-08-17 17:38:05 +0800

    Changes

---
+++ src/main/java/com/shf/spring/sms/task/Relationship/GenerateTable.java
@@ -0,0 +1,11 @@
+package com.shf.spring.sms.task.Relationship;
+import java.util.HashMap;
+
+public class GenerateTable {
+    public static String TableName;//表名
+    public HashMap<String, String[]> ContainsRelationMap;//<包含的列,计算需要的列>
+    public static String[] GenerateCols;//该表计算得到的列（可以扩展该结构）
+
+
+
+}
