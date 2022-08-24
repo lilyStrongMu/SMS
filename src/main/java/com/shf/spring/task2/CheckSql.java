156e9a74055973f2f8c8f86af1465fd785aaee21
gaolili <904513356@qq.com>
2022-08-24 17:59:23 +0800

    bugfix:项目启动失败

--- src/main/java/com/shf/spring/task2/CheckSql.java
+++ src/main/java/com/shf/spring/task2/CheckSql.java
@@ -1,10 +1,10 @@
 package com.shf.spring.task2;
 
-import com.shf.spring.sms.dto.checkNode;
 
-import java.sql.Wrapper;
+import com.shf.spring.sms.dto.CheckNode;
+
 import java.util.List;
 
 public interface CheckSql {
-    List<checkNode> getNewWeight(List<String> Sql);
+    List<CheckNode> getNewWeight(List<String> Sql);
 }
--- src/main/java/com/shf/spring/task2/Layer.java
+++ src/main/java/com/shf/spring/task2/Layer.java
@@ -1,12 +1,12 @@
 package com.shf.spring.task2;
 
-import com.shf.spring.sms.dto.checkNode;
+import com.shf.spring.sms.dto.CheckNode;
 
 import java.util.List;
 
 public class Layer implements CheckSql{
     @Override
-    public List<checkNode> getNewWeight(List<String> Sql) {
+    public List<CheckNode> getNewWeight(List<String> Sql) {
         return null;
     }
 }
